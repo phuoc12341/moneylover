846eea2e721e20366a97cca9e61eaf7afa4dfc8f
FRAMGIA\kieu.minh.phuoc@b121647 <phuoc123411@gmail.com>
2019-05-23 17:10:42 +0700

    build_slide_5_6_7

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
     new fullpage('#fullpage', {
         anchors:['firstPage', 'secondPage', 'thirdPage'],
-        scrollBar: true,
+        // scrollBar: true,
         scrollOverflowReset: false,
         controlArrows: true,
+
 
         afterLoad: function(origin, destination, direction){
             if (origin != null) {
@@ -38,25 +39,34 @@ $( document ).ready(function() {
             $destinationIndicator.addClass('active');
 
             let destinationContent = $(destination.item).find(".content")
-            destinationContent.removeClass('d-none');
-
+            if (destinationContent.hasClass("d-none")) {
+                destinationContent.removeClass('d-none');
+            }
         }
     });
 
     fullpage_api.setAllowScrolling(true);
 
+    let listContentOfSlide = $(".content");
+    listContentOfSlide.each(function( index, value ) {
+        if (index != 0) {
+            $(this).addClass("d-none");
+        }
+    });
+    console.log(listContentOfSlide)
+
     var owl = $('.owl-carousel');
     owl.owlCarousel({
         items:1,
-        loop:true,
         margin:10,
         autoplay:true,
         autoplayTimeout:3000,
         center: true,
+        rewind: true,
         autoplayHoverPause:true
     });
     $('.play').on('click',function(){
-        owl.trigger('play.owl.autoplay',[1000])
+        owl.trigger('play.owl.autoplay')
     })
     $('.stop').on('click',function(){
         owl.trigger('stop.owl.autoplay')
@@ -108,42 +118,56 @@ $( document ).ready(function() {
         });
     });
 
-    
-            $('.owl-next').click(function () {
-                owl.trigger('next.owl.carousel');
-            });
+    $('.owl-next').click(function () {
+        owl.trigger('next.owl.carousel', [400]);
+    });
 
-            $('.owl-prev').click(function () {
-                owl.trigger('prev.owl.carousel');
-            });
+    $('.owl-prev').click(function () {
+        owl.trigger('prev.owl.carousel', [400]);
+    });
 
-            $('.owl-page').click(function () {
-                indexSlideIsClicked = $(this).data("index-page");
-                switch (indexSlideIsClicked) {
-                    case 1:
-                        $("#section_4").find(".owl-page").addClass("");
-                        $("#section_4").find(".owl-page:nth-child(" + indexSlideIsClicked + ")").addClass("visible");
-                        break;
-                    case 2:
-                        // statements_1
-                        break;
-                    case 3:
-                        // statements_1
-                        break;
-                    default:
-                        // statements_def
-                        break;
-                }
-            })
+    $("#section_4").find(".owl-page:nth-child(1)").addClass("isClicked isShowed");
 
-            $("#section_4").find(".my-carousel").mouseenter(function () {
-                owl.trigger("stop.owl.autoplay");
-            });
+    $('.owl-page').click(function () {
+        indexImageIsClicked = $(this).data("index-page");
+        $("#section_4").find(".owl-page").removeClass("isClicked isShowed");
+        $("#section_4").find(".owl-page:nth-child(" + indexImageIsClicked + ")").addClass("isClicked isShowed");
+        
+        $("#section_4").find(".line-indicator").removeClass("firstImage secondImage thirdImage");
+        if (indexImageIsClicked == 1) {
+            $("#section_4").find(".line-indicator").addClass("firstImage");
+        } else if (indexImageIsClicked == 2) {
+            $("#section_4").find(".line-indicator").addClass("secondImage");
+        } else if (indexImageIsClicked == 3) {
+            $("#section_4").find(".line-indicator").addClass("thirdImage");
+        }
 
-            $("#section_4").find(".my-carousel").mouseleave(function () {
-                owl.trigger("play.owl.autoplay");
-            });
+        owl.trigger('to.owl.carousel', [--indexImageIsClicked, [400]]);
+    });
 
-        });
+    owl.on('changed.owl.carousel', function(event) {
+        let indexOfSlide = event.item.index;
+        indexOfSlide = ++indexOfSlide;
+
+        $("#section_4").find(".owl-page").removeClass("isClicked isShowed");
+        $("#section_4").find(".owl-page:nth-child(" + indexOfSlide + ")").addClass("isClicked isShowed");
+        
+        $("#section_4").find(".line-indicator").removeClass("firstImage secondImage thirdImage");
+        if (indexOfSlide == 1) {
+            $("#section_4").find(".line-indicator").addClass("firstImage");
+        } else if (indexOfSlide == 2) {
+            $("#section_4").find(".line-indicator").addClass("secondImage");
+        } else if (indexOfSlide == 3) {
+            $("#section_4").find(".line-indicator").addClass("thirdImage");
+        }
+    })
+
+    $("#section_4").find(".my-carousel").mouseenter(function () {
+        owl.trigger("stop.owl.autoplay");
+    });
+
+    $("#section_4").find(".my-carousel").mouseleave(function () {
+        owl.trigger("play.owl.autoplay");
+    });
 
 });
