53a94884d3e35d6416ee2a0b48f70a9aa8451212
FRAMGIA\kieu.minh.phuoc@b121647 <phuoc123411@gmail.com>
2019-05-28 14:46:40 +0700

    create_layout_admin

---
+++ database/migrations/2019_05_28_054441_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('logo');
+            $table->string('site_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->json('social');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
