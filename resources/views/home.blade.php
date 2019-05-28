bc41831881081edd3769189758498dd60883e480
FRAMGIA\kieu.minh.phuoc@b121647 <phuoc123411@gmail.com>
2019-05-28 17:21:42 +0700

    create_admin_setting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,87 @@
 @extends('layouts.app')
 
+@section('style')
+    <link href="css/fontawesome-iconpicker.min.css" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="row">
+        <div class="col-lg-12">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <!--begin::Portlet-->
+            <div class="m-portlet">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <span class="m-portlet__head-icon m--hide">
+                                <i class="la la-gear"></i>
+                            </span>
+                            <h3 class="m-portlet__head-text">
+                                Form Update Setting 
+                            </h3>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
+
+                <!--begin::Form-->
+                <form class="m-form" action="/setting" method="POST">
+                    @csrf
+                    <div class="m-portlet__body">
+                        <div class="m-form__section m-form__section--first">
+                            <div class="form-group m-form__group">
+                                <label for="example_input_full_name">Site name:</label>
+                                <input type="text" name="site_name" class="form-control m-input" placeholder="Enter site name">
+                                <span class="m-form__help">Please enter your site name</span>
+                            </div>
+                            <div class="form-group m-form__group">
+                                <label>Email address:</label>
+                                <input type="email" name="email" class="form-control m-input" placeholder="Enter email">
+                                <span class="m-form__help">We'll never share your email with anyone else</span>
+                            </div>
+                            <div class="form-group m-form__group">
+                                <label>Phone:</label>
+                                <input type="text" name="phone" class="form-control m-input" placeholder="Enter phone">
+                            </div>
+                            <div class="form-group m-form__group">
+                                <label>Address:</label>
+                                <input type="text" name="address" class="form-control m-input" placeholder="Enter address">
+                            </div>
+                            <div class="form-group m-form__group">
+                                <label>Logo:</label>
+                                <input type="file" name="logo" class="form-control m-input" placeholder="Enter Image for Logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet__foot m-portlet__foot--fit">
+                        <div class="m-form__actions m-form__actions">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="reset" class="btn btn-secondary">Cancel</button>
+                        </div>
+                    </div>
+
+                    <input class="icp demo" value="fa-anchor" type="text">
+                    <div class="btn-group">
+                      <button data-selected="graduation-cap" type="button" class="icp demo btn btn-default dropdown-toggle iconpicker-component" data-toggle="dropdown">
+                          Dropdown  <i class="fa fa-fw"></i>
+                          <span class="caret"></span>
+                      </button>
+                      <div class="dropdown-menu"></div>
+                    </div>
+
+                </form>
+
+                <!--end::Form-->
             </div>
+
+            <!--end::Portlet-->
+
         </div>
     </div>
-</div>
+@endsection
+
+@section('script')
+    <script src="js/fontawesome-iconpicker.js"></script>
+    <script>
+        $('.demo').iconpicker();
+    </script>
 @endsection
