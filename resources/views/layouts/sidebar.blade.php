070c0bad1b96b31f3742a9ef12577196857e052c
chu.minh.khai <FRAMGIA\chu.minh.khai@i120897-pc.framgia.com>
2019-06-04 13:47:34 +0700

    create slide 1

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,52 @@
+
+
+<div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
+    <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
+        <li class="m-menu__item  m-menu__item--active" aria-haspopup="true">
+            <a href="index.html" class="m-menu__link ">
+                <i class="m-menu__link-icon flaticon-line-graph"></i>
+                <span class="m-menu__link-title">
+                    <span class="m-menu__link-wrap">
+                        <span class="m-menu__link-text">Dashboard</span>
+                        <span class="m-menu__link-badge">
+                            <span class="m-badge m-badge--danger">2</span>
+                        </span>
+                    </span>
+                </span>
+            </a>
+        </li>
+        <li class="m-menu__section ">
+            <h4 class="m-menu__section-text">Components</h4>
+            <i class="m-menu__section-icon flaticon-more-v3"></i>
+        </li>
+        <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
+            <a href="{{ route('setting.create') }}" class="m-menu__link m-menu__toggle">
+                <i class="m-menu__link-icon flaticon-layers"></i>
+                <span class="m-menu__link-text">Setting</span>
+                <i class="m-menu__ver-arrow la la-angle-right"></i>
+            </a>
+        </li>
+        <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
+            <a href="{{ route('social.index') }}" class="m-menu__link m-menu__toggle">
+                <i class="m-menu__link-icon flaticon-layers"></i>
+                <span class="m-menu__link-text">Social</span>
+                <i class="m-menu__ver-arrow la la-angle-right"></i>
+            </a>
+        </li>
+        <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
+            <a href="{{ route('menu.index') }}" class="m-menu__link m-menu__toggle">
+                <i class="m-menu__link-icon flaticon-layers"></i>
+                <span class="m-menu__link-text">Menu</span>
+                <i class="m-menu__ver-arrow la la-angle-right"></i>
+            </a>
+        </li>
+        <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
+            <a href="{{ route('slide1.index') }}" class="m-menu__link m-menu__toggle">
+                <i class="m-menu__link-icon flaticon-layers"></i>
+                <span class="m-menu __link-text">Slide 1</span>
+                <i class="m-menu__ver-arrow la la-angle-right"></i>
+            </a>
+        </li>
+
+    </ul>
+</div>
