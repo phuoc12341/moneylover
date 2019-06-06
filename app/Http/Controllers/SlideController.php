67c4c9a3a31e3934eb705ebc43ddae0a8af3b0b6
chu.minh.khai <FRAMGIA\chu.minh.khai@i120897-pc.framgia.com>
2019-06-06 17:07:30 +0700

    CRUD alide 3, edit slide 4

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -67,7 +67,7 @@ class SlideController extends Controller
     {
         $slide = Slide::find($id);
         $slide->value = json_decode($slide->value);
-
+        // dd($slide->value);
         return view('slide.edit' . $id, ['slide' => $slide]);
     }
 
@@ -80,85 +80,146 @@ class SlideController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $slide = Slide::find($id);
+
         switch ($id) {
             case 1:
-                $slide = Slide::find($id);
-                $slide->value = json_decode($slide->value);
-                $arrImageOriginal = [];
-                $textLogoImageOriginal = '';
-        
-                if (isset($slide->value->image)) {
-                    $arrImageOriginal = $slide->value->image;
-                }
-        
-                if (isset($slide->value->text_logo)) {
-                    $textLogoImageOriginal = $slide->value->text_logo;
-                }
-        
-                $allRequestParameter = $request->all();
-                $allRequestParameter = Arr::except($allRequestParameter, ['_method', '_token', '_key', 'order']);
-                $allRequestParameter['image'] = $arrImageOriginal;
-                $allRequestParameter['text_logo'] = $textLogoImageOriginal;
-                if ($request->hasFile('image')) {
-                    foreach ($request->file('image') as $key => $image) {
-                        if ($image->isValid()) {
-                            $path = $image->store(config('custom.file_storage.upload_path'));
-                            $path = str_replace('public/', '', $path);
-                            if (isset($arrImageOriginal[$key])) {
-                                Storage::delete('public/' . $arrImageOriginal[$key]);
-                            }
+            $slide->value = json_decode($slide->value);
+            $arrImageOriginal = [];
+            $textLogoImageOriginal = '';
 
-                            $allRequestParameter['image'][$key] = $path;
+            if (isset($slide->value->image)) {
+                $arrImageOriginal = $slide->value->image;
+            }
 
-                            $allRequestParameter['image'] = array_replace($arrImageOriginal, $allRequestParameter['image']);
+            if (isset($slide->value->text_logo)) {
+                $textLogoImageOriginal = $slide->value->text_logo;
+            }
 
-                        }
-                    }
-                };
-
-                if ($request->hasFile('text_logo')) {
-                    if ($request->file('text_logo')->isValid()) {
-                        $textLogoImageRequest = $request->file('text_logo');
-                        $path = $textLogoImageRequest->store(config('custom.file_storage.upload_path'));
+            $allRequestParameter = $request->all();
+            $allRequestParameter = Arr::except($allRequestParameter, ['_method', '_token', '_key', 'order']);
+            $allRequestParameter['image'] = $arrImageOriginal;
+            $allRequestParameter['text_logo'] = $textLogoImageOriginal;
+            if ($request->hasFile('image')) {
+                foreach ($request->file('image') as $key => $image) {
+                    if ($image->isValid()) {
+                        $path = $image->store(config('custom.file_storage.upload_path'));
                         $path = str_replace('public/', '', $path);
-
-                        if (empty($textLogoImageOriginal)) {
-                            Storage::delete('public/' . $textLogoImageOriginal);
+                        if (isset($arrImageOriginal[$key])) {
+                            Storage::delete('public/' . $arrImageOriginal[$key]);
                         }
 
-                        $allRequestParameter['text_logo'] = $path;
-                    }
-                };
+                        $allRequestParameter['image'][$key] = $path;
 
-                break;
-            case 2:
-                $allRequestParameter = $request->all();
-                if ($request->hasFile('file')) {
-                    $file = $request->file;
-                    $name = $request->file->getClientOriginalName();
-                    $name = str_random(5).$name;
-                    $file->move(config('app.img_path'), $name);
+                        $allRequestParameter['image'] = array_replace($arrImageOriginal, $allRequestParameter['image']);
+
+                    }
                 }
-                $file_1 = [];
-                if($request->hasFile('file_1')) {
-                    foreach ($request->file('file_1') as $item) {
-                        $name = $item->getClientOriginalName();
-                        $newName = str_random(5) . $name;
-                        $item->move(config('app.img_path'), $newName);
+            };
+
+            if ($request->hasFile('text_logo')) {
+                if ($request->file('text_logo')->isValid()) {
+                    $textLogoImageRequest = $request->file('text_logo');
+                    $path = $textLogoImageRequest->store(config('custom.file_storage.upload_path'));
+                    $path = str_replace('public/', '', $path);
+
+                    if (empty($textLogoImageOriginal)) {
+                        Storage::delete('public/' . $textLogoImageOriginal);
+                    }
+
+                    $allRequestParameter['text_logo'] = $path;
+                }
+            };
+
+            break;
+            case 2:
+            $allRequestParameter = $request->all();
+            $allRequestParameter = Arr::except($allRequestParameter, ['_method', '_token', '_key', 'order']);
+
+            break;
+            case 3:
+            $slide->value = json_decode($slide->value);
+            $saveImage = '';
+            if (isset($slide->value->file)){
+                $saveImage = $slide->value->file;
+            }
+
+            $allRequestParameter = $request->all();
+            $allRequestParameter = Arr::except($allRequestParameter, ['_method', '_token', '_key', 'order']);
+            $allRequestParameter['file'] = $saveImage;
+            if ($request->hasFile('file')) {
+                $file = $request->file;
+                $name = $request->file->getClientOriginalName();
+                $nameFile = str_random(5).$name;
+                $file->move(config('app.img_path'), $nameFile);
+                if (isset($slide->value->file)) {
+                    unlink(config('app.img_path') . $slide->value->file);
+                }
+                $allRequestParameter['file'] = $nameFile;
+            }
+
+            $file_1 = [];
+            $saveImage_1 = [];
+            if (isset($slide->value->file_1)) {
+                $saveImage_1 = $slide->value->file_1;
+            }
+            $allRequestParameter['file_1'] = $saveImage_1;
+            // dd($slide->value->file_1);
+            if($request->hasFile('file_1')) {
+                foreach ($request->file('file_1') as $key => $item) {
+                    $name = $item->getClientOriginalName();
+                    $newName = str_random(5) . $name;
+                    $item->move(config('app.img_path'), $newName);
+                    if (isset($slide->value->file_1)) {
+                        foreach ($slide->value->file_1 as $item) {
+                            if (isset($item->img)) {
+                                unlink(config('app.img_path') . $item->img);
+                            }
+                        }
+
                         $file_1[] = ['img' => $newName];
                     }
-                            }
-                $allRequestParameter = Arr::except($allRequestParameter, ['_method', '_token', '_key', 'order']);
-                $allRequestParameter = array_replace($allRequestParameter, ['file_1' => $file_1]);
-                $allRequestParameter = array_replace($allRequestParameter, ['file' => $name]);
+                }
+            }
+            $allRequestParameter['file_1'] = array_replace($saveImage_1, $file_1);
 
-                break;
+            $file_2 = [];
+            $saveImage_2 = [];
+            if (isset($slide->value->file_2)) {
+                $saveImage_2 = $slide->value->file_2;
+            }
+            $allRequestParameter['file_2'] = $saveImage_2;
+            if($request->hasFile('file_2')) {
+                foreach ($request->file('file_2') as $item) {
+                    $name = $item->getClientOriginalName();
+                    $newName = str_random(5) . $name;
+                    $item->move(config('app.img_path'), $newName);
+                    if (isset($slide->value->file_2)) {
+                        foreach ($slide->value->file_2 as $key => $item) {
+                            if (isset($item->img)) {
+                                unlink(config('app.img_path') . $item->img);
+                            }
+                        }
+                        $file_2[] = ['img' => $newName];
+                    }
+                }
+            }
+
+            $allRequestParameter['file_2'] = array_replace($saveImage_2, $file_2);
+            $allRequestParameter = Arr::except($allRequestParameter, ['_method', '_token', '_key', 'order']);
+            
+            break ;
+            case 4:
+                dd($request->all());
+            break;
+
             default:
 
-                break;
+            break;
         }
 
         $valueJsonEncoded = json_encode($allRequestParameter);
+            // dd($valueJsonEncoded);
         $slide->key = $request->key;
         $slide->value = $valueJsonEncoded;
 
